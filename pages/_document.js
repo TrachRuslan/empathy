35a54feb5c38922f0d324d1e5087d4a37738d8ea
TrachRuslan <pyclanhero@gmail.com>
2025-12-03 03:49:23 +0200

    Add Google Search Console verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="uk-UA">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="W6N7Z0XpbAbX4lp5RAmidCswjU2OGZdjm0NSLoh_Zl0"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
